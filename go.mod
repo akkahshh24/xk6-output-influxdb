ae92bde9ac391b6d3aa53eb8994bd1fd29970cb9
akkahshh.a <akkahshh.a@npci.org.in>
2023-10-26 12:56:53 +0530

    add xmlsigner extension

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/grafana/xk6-output-influxdb
 go 1.19
 
 require (
+	github.com/akkahshh24/go-xml-signer v0.0.0-20231026054539-21d3455fab2b
+	github.com/beevik/etree v1.2.0
+	github.com/brianvoe/gofakeit/v6 v6.24.0
 	github.com/influxdata/influxdb-client-go/v2 v2.12.2
 	github.com/mstoykov/envconfig v1.4.1-0.20220114105314-765c6d8c76f1
 	github.com/sirupsen/logrus v1.9.0
-	github.com/stretchr/testify v1.8.2
+	github.com/stretchr/testify v1.8.4
 	go.k6.io/k6 v0.45.1
+	golang.org/x/crypto v0.14.0
 	gopkg.in/guregu/null.v3 v3.5.0
 )
 
@@ -15,22 +19,28 @@ require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deepmap/oapi-codegen v1.12.4 // indirect
+	github.com/dlclark/regexp2 v1.9.0 // indirect
+	github.com/dop251/goja v0.0.0-20230531210528-d7324b2d74f7 // indirect
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.4-0.20211119122758-180fcef48034+incompatible // indirect
+	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20210922203350-b1ad95c89adf // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mstoykov/atlas v0.0.0-20220811071828-388f114305dd // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.29.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/serenize/snaker v0.0.0-20201027110005-a7ad2135616e // indirect
 	github.com/spf13/afero v1.9.5 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ xmlsigner/xml_signer.go
@@ -0,0 +1,89 @@
+// xk6 build latest --with github.com/akkahshh24/xk6-xml-signer@v0.0.7 --with github.com/grafana/xk6-output-influxdb@latest --output k6-xmlsigner-influxdb
+
+package xmlsigner
+
+import (
+	"crypto"
+	"crypto/rsa"
+	"encoding/pem"
+	"log"
+	"os"
+	"time"
+
+	dsig "github.com/akkahshh24/go-xml-signer"
+	"github.com/beevik/etree"
+	"github.com/brianvoe/gofakeit/v6"
+	"go.k6.io/k6/js/modules"
+	"golang.org/x/crypto/pkcs12"
+)
+
+func init() {
+	modules.Register("k6/x/xmlsigner", new(XmlSigner))
+}
+
+type XmlSigner struct {
+	PrivateKey crypto.Signer
+	CertBytes  []byte
+	SignedXml  string
+	TxnId      string
+}
+
+func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) {
+	p12Bytes, err := os.ReadFile(p12FilePath)
+	if err != nil {
+		log.Fatalf("failed to read .p12 file: %v", err)
+	}
+
+	privateKey, cert, err := pkcs12.Decode(p12Bytes, password)
+	if err != nil {
+		log.Fatalf("failed to decode .p12 file: %v", err)
+	}
+
+	rsaPrivateKey, ok := privateKey.(*rsa.PrivateKey)
+	if !ok {
+		log.Fatalf("private key is not of type *rsa.PrivateKey")
+	}
+
+	signer := crypto.Signer(rsaPrivateKey)
+
+	certPEM := &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert.Raw,
+	}
+
+	certPEMBytes := pem.EncodeToMemory(certPEM)
+
+	x.PrivateKey = signer
+	x.CertBytes = certPEMBytes
+}
+
+func (x *XmlSigner) GetSignedXml(payload string) {
+
+	doc := etree.NewDocument()
+	if err := doc.ReadFromString(payload); err != nil {
+		panic(err)
+	}
+
+	doc.Root().SelectElement("Head").SelectAttr("msgId").Value = gofakeit.Regex(`^[a-zA-Z0-9]{35}$`)
+	doc.Root().SelectElement("Head").SelectAttr("ts").Value = time.Now().Format("2006-01-02T15:04:05.000-07:00")
+	doc.Root().SelectElement("Txn").SelectAttr("custRef").Value = gofakeit.Regex(`^[0-9]{12}$`)
+	x.TxnId = gofakeit.Regex(`^[a-zA-Z0-9]{35}$`)
+	doc.Root().SelectElement("Txn").SelectAttr("id").Value = x.TxnId
+	doc.Root().SelectElement("Txn").SelectAttr("refId").Value = gofakeit.Regex(`^[0-9]{6}$`)
+	doc.Root().SelectElement("Txn").SelectAttr("ts").Value = time.Now().Format("2006-01-02T15:04:05.000-07:00")
+
+	ctx, _ := dsig.NewSigningContext(x.PrivateKey, [][]byte{x.CertBytes})
+	signedElement, err := ctx.SignEnveloped(doc.Root())
+	if err != nil {
+		log.Fatalf("failed to sign payload: %v", err)
+	}
+
+	doc.SetRoot(signedElement)
+
+	str, err := doc.WriteToString()
+	if err != nil {
+		log.Fatalf("failed to write payload to string: %v", err)
+	}
+
+	x.SignedXml = str
+}

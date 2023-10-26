bfea91c9676edfcafc7a2fb398138f2735c6527a
akkahshh.a <akkahshh.a@npci.org.in>
2023-10-26 13:14:19 +0530

    remove init from xmlsigner

--- register.go
+++ register.go
@@ -3,10 +3,13 @@ package influxdb
 
 import (
 	"github.com/akkahshh24/xk6-output-influxdb/pkg/influxdb"
+	"github.com/akkahshh24/xk6-output-influxdb/xmlsigner"
+	"go.k6.io/k6/js/modules"
 	"go.k6.io/k6/output"
 )
 
 func init() {
+	modules.Register("k6/x/xmlsigner", new(xmlsigner.XmlSigner))
 	output.RegisterExtension("xk6-influxdb", func(p output.Params) (output.Output, error) {
 		return influxdb.New(p)
 	})
